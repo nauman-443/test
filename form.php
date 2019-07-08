44fc3d069ed768baab5e3a4d2c4dd747ee24ff02
nauman-443 <nauman443@gmail.com>
2019-07-08 22:35:47 +0500

    demo form

---
+++ form.php
@@ -0,0 +1,18 @@
+<html>
+<form action="process.php" method="get">
+	Name:
+	<input type="text" name="name">
+	<br>
+	Lastname:
+	<input type="text" name="Lastname">
+	<br>
+	Email:
+	<input type="text" name="Email">
+	<br>
+	Password:
+	<input type="Password" name="Password">
+	<br><br>
+	<input type="submit" name="submit">
+
+</form>
+</html>
